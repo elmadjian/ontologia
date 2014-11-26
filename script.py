3bd9749766aee78900176a0d75afc7d3ce0cb71c
elmadjian <cadu_elmadjian@yahoo.com.br>
2014-11-25 22:26:35 -0200

    - adicionado interpretador p dados do scriptlattes - ontologia ampliada (agora estou entendendo melhor)
    Por enquanto, o script abre um arquivo .ris, faz o parsing das regiões
    de interesse e associa um campo a essas regiões.
    
    Por exemplo:
    campo: autor --> Fulano da Silva Silveira
    
    Alguns campos podem ser multivalorados, então falta trabalhar nesse
    ponto. Precisamos, depois disso, usar esses campos para criar entradas
    na nossa ontoligia OWL.
    
    OBS.: script é para Python3: não roda em 2!

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-!#/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 '''================================================================
@@ -13,23 +13,53 @@ import sys
 import re
 
 #programa principal
+#=================================================================
 def main(argv):
-
     #lê arquivo de entrada .ris
     reader = Reader(argv)
-    filename = reader.getFilename
+    filename = reader.getFilename()
 
     with open(filename, 'r') as data:
+        for line in data:
+            runPattern(line)
 
 
 #interpreta padrão com expressões regulares
+#=================================================================
 def runPattern(line):
-    patterns
+    #padrões de busca regex
+    patterns = [
+
+    #padrões para o arquivo .ris de publicações
+    "(PY)\s*-\s*([\d]{4})", #ano de publicação
+    "(AU)\s*-\s*(.+)",      #autor
+    "(TI)\s*-\s*(.+)",      #título
+    "(JO)\s*-\s*(.+)",      #revista
+
+    #padrões para o arquivo .ris de currículo Lattes
+    "(NOME)\s*-\s*(.+)",    #nome completo
+    "(CITA)\s*-\s*(.+)",    #nome para citação (muitas ocorrências!)
+    "(FO)\d+d\s*-\s*(.+)"    #local onde estudou
+    ]
+
+    #dicionário para busca
+    hashmap = {
+    'PY':   "anoPublicacao",
+    'AU':   "autor",
+    'TI':   "titulo",
+    'JO':   "revista",
+    'NOME': "nome",
+    'CITA': "citacao",
+    'FO':   "localDeEstudo"
+    }
+
+    for pattern in patterns:
+        match = re.search(pattern, line)
+        if match:
+            print (hashmap[match.group(1)],"-->",match.group(2))
 
 
-#=========================#
-if __name__ == "__main__":
-    main(sys.argv)
+
 
 
 #||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
@@ -39,27 +69,34 @@ if __name__ == "__main__":
 #define a classe de leitura do arquivo problema.dat
 class Reader:
 
-#__init__(tuple) -> self
 #>>> construtor
+#__init__(tuple) -> self
 #=============================================================
-def __init__(self, argv):
-    #se houver mais de um nome como parâmetro do programa: ERRO
-    if len(argv) > 2:
-        print("ERRO: você chamou %s com mais de um parâmetro" % argv[0])
-        exit()
-    #se houver um parâmetro: salva o nome em self.filename
-    elif len(argv) == 2:
-        self.filename = argv[1]
-    #se não houver parâmetro, pede que o usuário digite o nome do arquivo
-    else:
-        self.filename = input("Por favor, digite o nome do arquivo para leitura:")
+    def __init__(self, argv):
+        #se houver mais de um nome como parâmetro do programa: ERRO
+        if len(argv) > 2:
+            print("ERRO: você chamou %s com mais de um parâmetro" % argv[0])
+            exit()
+        #se houver um parâmetro: salva o nome em self.filename
+        elif len(argv) == 2:
+            self.filename = argv[1]
+        #se não houver parâmetro, pede que o usuário digite o nome do arquivo
+        else:
+            self.filename = input("Por favor, digite o nome do arquivo para leitura:")
 
-    #se o arquivo não existir: ERRO
-    if not os.path.isfile(self.filename):
-        print("ERRO: arquivo %s inexistente" % self.fileName)
-        exit()
+        #se o arquivo não existir: ERRO
+        if not os.path.isfile(self.filename):
+            print("ERRO: arquivo %s inexistente" % self.fileName)
+            exit()
 
-#>>> devolve o nome do arquivo lido
-#==============================================================
-def getFilename(self):
-    return self.filename
+    #>>> devolve o nome do arquivo lido
+    #getFilename() -> self.filename
+    #==============================================================
+    def getFilename(self):
+        return self.filename
+
+
+
+#=========================#
+if __name__ == "__main__":
+    main(sys.argv)
